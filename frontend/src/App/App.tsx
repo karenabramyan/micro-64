0337927ce8b26b6301273976f60a8993415fb36c
Denis <AntonGorbenko7@gmail.com>
2022-10-20 19:31:16 +0300

    nav create

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import { Button } from '@mui/material';
+import Navigation from '../Navbar/Navigation';
 
 function App(): JSX.Element {
   return (
     <>
+      <Navigation />
       <h1>MICRO-64</h1>
       <Button variant="text">Text</Button>
 
