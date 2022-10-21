2304564607fcfb43f4ffbc7236969e55d831b17f
LidiyaLaletina <laletina77@mail.ru>
2022-10-21 19:21:53 +0300

    info-create

---
+++ frontend/src/features/Infopages/InfopagesList/types/Info.ts
@@ -0,0 +1,15 @@
+import Paper from './paper';
+import Root from './root';
+
+export default interface Info {
+  id: string;
+  title: string;
+  description: string;
+  // padding?: string;
+  // textAlign?:string;
+  // color?: string;
+  root?: Root;
+  paper?: Paper;
+}
+
+
