e0201f02d243e063207f69e4b4a792a4dfa00811
karenabramyan <ptp.abramyan@gmail.com>
2022-10-24 20:01:17 +0300

    add items by admin in admin cabinet

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,7 @@ const itemsRouter = require('./routes/itemsRoute');
 const regRouter = require('./routes/registrationRoute');
 const loginRouter = require('./routes/loginRoute');
 const basketRouter = require('./routes/basketRoute');
+const adminItemRouter = require('./routes/itemsFromAdminRoute');
 
 const PORT = process.env.PORT ?? 4000;
 const app = express();
@@ -21,6 +22,7 @@ app.use('/api/items', itemsRouter);
 app.use('/api/registration', regRouter);
 app.use('/api/auth', loginRouter);
 app.use('/api/basket', basketRouter);
+app.use('/api/admin-item', adminItemRouter);
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -14,6 +14,7 @@ import { getUser } from '../features/auth/authSlice';
 import Basket from '../features/basket/Basket';
 import InfoList from '../features/Infopages/InfopagesList/InfoList';
 import InfoPage from '../features/Infopages/InfopagesList/InfoPage';
+import AdminCab from '../features/adminCab/AdminCab';
 
 function App(): JSX.Element {
   const dispatch = useAppDispatch();
@@ -34,6 +35,7 @@ function App(): JSX.Element {
           <Route path="/login" element={<Login />} />
           <Route path="/basket" element={<Basket />} />
           <Route path="/info/infopage/:id" element={<InfoPage />} />
+          <Route path="/admincab" element={<AdminCab />} />
         </Route>
       </Routes>
     </>
