cfc75b25cd2ea06a3f9204760a91d9c0348e038c
Denis <AntonGorbenko7@gmail.com>
2022-10-24 17:38:23 +0300

    next/prev InfoPage

--- frontend/src/features/Infopages/InfopagesList/InfoPage.tsx
+++ frontend/src/features/Infopages/InfopagesList/InfoPage.tsx
@@ -1,14 +1,46 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { infoArr } from './InfoArr';
 
 function InfoPage(): JSX.Element {
     const { id } = useParams();
+    // const [count, setCount] = useState(Number(id));
+    // console.log(count);
+    
+    // function handleClicPlus():void {
+    //     if(count >= 5){
+    //         setCount(1)
+    //     }else if(count <= 0){
+    //         setCount(5)
+    //     }else{
+    //         setCount((x:number) => x + 1);
+    //     }
+    // }
+    // function handleClicMinus():void {
+    //     if(count >= 5){
+    //         setCount(1)
+    //     }else if(count <= 1){
+    //         setCount(5)
+    //     }else{
+    //         setCount((x:number) => x - 1);
+    //     }
+    // }
+    const navigate = useNavigate();
+    function navigatePage(way: string) {
+        if(Number(way) >= 6){
+            navigate(`/info/infopage/${1}`);
+        }else if(Number(way) <= 0){
+            navigate(`/info/infopage/${5}`);
+        }else{
+            navigate(`/info/infopage/${way}`);
+        }
+    }
     const oneItem = infoArr.filter((el) => el.id === Number(id));
     return (
         <div className="container">
             <div className="topCont">{oneItem[0].name}</div>
             <div className="botCont">{oneItem[0].bodyTwo}{oneItem[0].bodyOne}</div>
+            <button onClick={()=> navigatePage(`${Number(id)-1}`)}>Предыдущая статья</button><button onClick={()=> navigatePage(`${Number(id) + 1}`)}>Следующая статья</button>
         </div>
     );
 }
