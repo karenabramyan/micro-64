b02175c7a74b627a08466bd7e27815528ae533c4
juantonova <j.konina@mail.ru>
2022-10-25 19:09:54 +0300

    Orders created

--- frontend/src/features/adminCab/AdminOrders.tsx
+++ frontend/src/features/adminCab/AdminOrders.tsx
@@ -1,20 +1,34 @@
-import { Typography } from '@mui/material';
-import React from 'react';
+import { Card, Typography } from '@mui/material';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState, useAppDispatch } from '../../store';
+import { loadOrders } from './adminSlice';
+import OrderForAdmin from './Order/OrderForAdmin';
 // import { useSelector } from 'react-redux';
 
 function AdminOrders(): JSX.Element {
-    // const dispatch = useAppDispatch();
+    const dispatch = useAppDispatch();
 
-    // const items = useSelector(selectItems);
+    const orders = useSelector((state: RootState) => state.orders.orders);
 
-    // useEffect(() => {
-    //     dispatch(loadCards());
-    //   }, [dispatch]);
+    useEffect(() => {
+        dispatch(loadOrders());
+      }, [dispatch]);
 
   return (
     <div className="container">
         <Typography variant="h5">Заказы клиентов</Typography>
-    {/* {items.map((item) => <Item item={item} key={item.id} />)} */}
+        <br />
+        <Card className="basket-item">
+          <Typography className="admin-user-id" variant="h6">ID</Typography>
+          <Typography className="admin-user-phone" variant="h6">Телефон</Typography>
+          <Typography className="admin-item-title" variant="h6">Название товара</Typography>
+          <Typography className="admin-order-days" variant="h6">Дни</Typography>
+          <Typography className="admin-order-type" variant="h6">Тип заказа</Typography>
+          <Typography className="admin-order-price" variant="h6">Цена</Typography>
+          <Typography className="admin-order-status" variant="h6">Статус заказа</Typography>
+        </Card>
+    {orders.map((order) => <OrderForAdmin order={order} key={order.itemTitle} />)}
     </div>
   );
 }
--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -3,12 +3,14 @@ import { useDispatch } from 'react-redux';
 import authSlice from './features/auth/authSlice';
 import cardSlice from './features/cards/itemSlice';
 import basketSlice from './features/basket/basketSlice';
+import adminSlice from './features/adminCab/adminSlice';
 
 const store = configureStore({
     reducer: {
         items: cardSlice,
         auth: authSlice,
         basket: basketSlice,
+        orders: adminSlice,
     }
 });
 export type RootState = ReturnType<typeof store.getState>;
